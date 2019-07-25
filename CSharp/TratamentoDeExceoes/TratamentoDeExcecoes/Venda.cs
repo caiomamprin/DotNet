a78df82f7f4a4e4e57e11fde7d80cac7c1c73580
caiomamprin <caiomamprin7@gmail.com>
2019-07-25 00:36:07 -0300

    Exemplos para treinar tratamento de exceção genérica, pelo tipo, nativas, múltiplas eceções, exceção customizada criando uma classe que herda a Exception e o bloco Finally

---
+++ CSharp/TratamentoDeExceoes/TratamentoDeExcecoes/Venda.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TratamentoDeExcecoes
+{
+    public class Venda
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+            /*Trecho de Código que Gera excessão por não estar instanciado*/
+        //public List<ItemVenda> Itens { get; } ;
+        public List<ItemVenda> Itens { get; } = new List<ItemVenda>();
+
+        public void AdicionarItem(ItemVenda item)
+        {
+            if (item == null)
+                throw new VendaException(999, "O item não pode ser nulo");
+            Itens.Add(item);
+        }
+
+        public decimal CalcularTotal()
+        {
+            return Itens.Sum(i => i.Preco * i.Quantidade);
+        }
+    }
+}
