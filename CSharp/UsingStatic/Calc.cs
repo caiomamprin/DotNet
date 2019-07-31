33e1d7fda8529dbd32f8cb16ef90645c9b6072a7
caiomamprin <caiomamprin7@gmail.com>
2019-07-31 00:11:32 -0300

    Exemplo de como trabalhar com  com a cláusula using static, permitindo definir na própria sessão de namespaces using a importação de uma classe estática ou parcialmente estática, e não só namespaces, como por exemplo Console, Convert entre outras

---
+++ CSharp/UsingStatic/Calc.cs
@@ -0,0 +1,10 @@
+﻿namespace Util
+{
+    public static class Calc
+    {
+        public static int Somar(int X, int Y)
+        {
+            return X + Y;
+        }
+    }
+}
---
+++ CSharp/UsingStatic/Program.cs
@@ -0,0 +1,15 @@
+﻿using static Util.Calc;
+using static System.Console;
+
+namespace UsingStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = Somar(3, 5);
+            WriteLine(r);
+            ReadLine();
+        }
+    }
+}
