df79015a9d5ec9d7b97ab6e9f758683f76ce9c62
caiomamprin <caiomamprin7@gmail.com>
2019-07-31 00:14:08 -0300

    Exemplo de como trabalhar com o recurso de string interpolation (interpolação de strings) , para facilitar a manipulação de String, facilitando caso precise colocar valores através de variáveis, métodos criando strings de forma dinâmica

---
+++ CSharp/StringInterpolation/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using static System.DateTime;
+
+namespace StringInterpolation
+{
+    public class Pessoa
+    {
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Pessoa() {
+                Matricula = 123,
+                Nome = "Guinther",
+                SobreNome = "Pauli" };
+            // WriteLine("Bem-vindo " + p.Nome + " " + p.SobreNome + "(" +
+            // p.Matricula + "), este é o C# 6");
+            //WriteLine(string.Format("Bem-vindo {0} {1} ({2}), este é o C# 6",
+            //p.Nome, p.SobreNome, p.Matricula));
+            WriteLine($"Bem-vindo {p.Nome, 10} de B. {p.SobreNome, 10} ({p.Matricula}), este é o C# 6");
+            WriteLine($"Olá, agora são {Now.ToLongTimeString()}");
+            ReadLine();
+        }
+    }
+}
