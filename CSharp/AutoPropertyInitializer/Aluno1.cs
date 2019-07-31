30336b20a4332f9eed1ef067ce32926553f5c6ca
caiomamprin <caiomamprin7@gmail.com>
2019-07-31 00:16:59 -0300

    Exemplo de como trabalhar com o recurso de auto property initializer (inicialização de propriedades automáticas). Com esse recurso é possível deixar o código bastante enxuto informando o valor default para uma propriedade diretamente na sua declaração.

---
+++ CSharp/AutoPropertyInitializer/Aluno1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Aluno1
+{
+    private int _matricula;
+    private string _nome;
+
+    public Aluno1(int matricula, string nome)
+    {
+        this._matricula = matricula;
+        this._nome = nome;
+    }
+
+    public Aluno1()
+    {
+        this._matricula = 0;
+        this._nome = "sem nome";
+    }
+
+    public int Matricula
+    {
+        get { return this._matricula; }
+        set { this._matricula = value; }
+    }
+    public string Nome
+    {
+        get { return this._nome; }
+        set { this._nome = value; }
+    }
+  
+
+}
---
+++ CSharp/AutoPropertyInitializer/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+namespace AutoPropertyInitializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gp = new Aluno(); // { Matricula = 123, Nome = "Guinther" };
+            WriteLine($"{ Aluno.Matricula} - {gp.Nome} - {gp.Status}");
+            ReadLine();
+        }
+    }
+}
