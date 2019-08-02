7b5a49c6a76ebe9e28e0355baba2e534aa77bd90
caiomamprin <caiomamprin7@gmail.com>
2019-08-01 23:20:25 -0300

    Exemplo didatico(por isso as classes não estão separadas, para agilizar o exemplo) de como trabalhar com o   recurso de ExpressionBodies, permitindo implementar o corpo de um método ou inicialização de uma propriedade variável como se fosse uma expressão lambda. Expressões lambda até então eram usadas para representar implementação de métodos anônimos sem um nome definido para sua assinatura. Agora, a própria implementação do corpo do método, que iria entre o bloco { }, pode ser feita através da mesma sintaxe de lambda expressions, como uma atribuição =>.

---
+++ CSharp/ExpressionBodies/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace ExpressionBodies
+{
+    public static class Calculadora
+    {
+        public static string Nome => "SuperCalc";
+        public static int Somar(int X, int Y) => X + Y;
+        //{
+        //    return X + Y;
+        //}
+        public static int Multiplicar(int X, int Y) => X * Y;
+        //{
+        //    return X + Y;
+        //}
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            var r1 = Calculadora.Somar(x, y);
+            WriteLine($"Usando a calculadora {Calculadora.Nome} Soma:{r1}");
+            x = 3;
+            y = 7;
+            var r2 = Calculadora.Multiplicar(x, y);
+            WriteLine($"Usando {Calculadora.Nome}, a multiplicação de {x} e {y} é {r2}");
+        }
+    }
+}
