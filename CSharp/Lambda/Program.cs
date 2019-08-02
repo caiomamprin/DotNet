ef6b99eb96608d11fa311417f553c5b585e98f84
caiomamprin <caiomamprin7@gmail.com>
2019-08-01 23:18:47 -0300

    Exemplo didatico(por isso as classes não estão separadas, para agilizar o exemplo) de como trabalhar com o   operador Lambda, para facilitar o entendimento do recurso de ExpressionBodies.

---
+++ CSharp/Lambda/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace ExpressionBodies
+{
+    public delegate void PagarEvent (double valor);
+
+    public class Pedido
+    {
+        public event PagarEvent Pagar;
+
+        public void Fechar(double valor)
+        {
+            Pagar(valor);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pedido = new Pedido();
+            pedido.Pagar += v => WriteLine($"Pago valor de {v}");
+            pedido.Fechar(2000); 
+        }
+
+        //private static void Pedido_Pagar(double valor)
+        //{
+        //    WriteLine($"Pago valor de {valor}");
+        //}
+    }
+}
