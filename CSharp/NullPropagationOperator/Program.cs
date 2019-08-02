ca37126a419fb4762a072e01fe463c9c022b5f23
caiomamprin <caiomamprin7@gmail.com>
2019-08-01 23:11:00 -0300

    Exemplo didatico(por isso as classes não estão separadas para agilizar o exemplo) de como trabalhar com o  recurso de Null Conditional Operator / Null Propagation Operator. Como o erro de tentar acessar um método/propriedade de um objeto que não foi estanciado ocorre muito, e as vezes é complicado para encontrar o erro, esse recurso ajuda bastante, e deixa o código mais legível sem muito if para fazer essa verificação.

---
+++ CSharp/NullPropagationOperator/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+namespace NullPropagationOperator
+{
+    public class Aluno
+    {
+        public int Matricula { get; set; } = 0;
+        public string Nome { get; set; } = "sem nome";
+        public Curso Curso { get; set; }
+    }
+
+    public class Curso
+    {
+        public int Codigo { get; set; } = 0;
+        public string NomeCurso { get; set; } = "sem nome";
+        public Area Area { get; set; }
+    }
+
+    public class Area
+    {
+        public string NomeArea { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gp = new Aluno() {
+                Matricula = 123,
+                Nome = "Guinther",
+                Curso = new Curso() {
+                    NomeCurso = "Sistemas de Informação",
+                    Area = new Area() { NomeArea = "Ciências Exatas" }
+                }
+            };
+            WriteLine($"Matricula:{gp.Matricula}\nNome:{gp.Nome}");
+            WriteLine($"Curso:{gp?.Curso?.NomeCurso ?? "Sem Curso"}");
+            WriteLine($"Área:{gp?.Curso?.Area?.NomeArea ?? "Sem Área"}");
+            ReadLine();
+        }
+    }
+}
---
+++ CSharp/NullPropagationOperator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NullPropagationOperator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NullPropagationOperator")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("13f620fd-c9c8-4921-a832-3a7747176063")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
