98dc9bc60f1012f4796aeefd5b16f3c9e393bd7d
caiomamprin <caiomamprin7@gmail.com>
2019-08-01 23:16:43 -0300

    Exemplo didatico(por isso as classes não estão separadas, para agilizar o exemplo) de como trabalhar com o   operador nameof. Esse operador recebe como parâmetro uma referência a um objeto, propriedade e variável, e retorna o seu nome em string, mesmo que o objeto, propriedade, atributo ou variável tenha um valor nulo (null), o que geraria um NullReferenceException, que podemos ser usados por exemplo para informar em tela que não foram passados corretamente.

---
+++ CSharp/NameOfOperator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+namespace NameOfOperator
+{
+    public class Professor
+    {
+        public string Matricula { get; set; } = "0";
+        public string Nome { get; set; } = "sem nome";
+        public DateTime Data { get; set; } = DateTime.Now;
+        public override string ToString()
+        {
+            return $"Matricula:{Matricula},Nome:{Nome},Data:{Data}";
+        }
+    }
+
+    public static class Relatorio
+    {
+        public static void Imprimir(Professor prof)
+        {
+            WriteLine("Relatório:");
+            WriteLine(prof?.ToString() ?? 
+                $"O parâmetro de nome {nameof(prof)} não pode ser nulo");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Professor GBP = null;
+            Relatorio.Imprimir(GBP);
+            WriteLine(GBP?.ToString() ?? $"Objeto {nameof(GBP)} não instanciado");
+            GBP = new Professor() { Matricula = "123", Nome = "Guinther" };
+            WriteLine(GBP);
+            Relatorio.Imprimir(GBP);
+        }
+    }
+}
