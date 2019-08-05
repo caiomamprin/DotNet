046abea29346a80772138e7dc30e479690fe8487
caiomamprin <caiomamprin7@gmail.com>
2019-08-04 22:58:23 -0300

    Exemplo didatico(por isso as classes não estão separadas, para agilizar o exemplo) de como trabalhar com o   recurso de exception filters , permitindo que você construa um bloco try catch usando a diretiva WHEN, para filtar uma exceção, de acordo com uma condição, o que antes só era possível usando um catch e dentro dele um ou mais IFs. Um catch só pode manipular ou capturar uma determinada exceção pelo seu tipo, por exemplo, FormatException.

---
+++ CSharp/ExceptionFilters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+
+namespace ExceptionFilters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 999999999;
+            int Y = 0;
+            var R1 = 0;
+            int R2 = 0;
+            try
+            {
+                R1 = X / Y;
+                WriteLine($"{X} dividido por {Y} é igual a {R1}");
+                R2 = checked(X * Y);
+                WriteLine($"{X} multiplicado por {Y} é igual a {R2}");
+            }
+            //catch (DivideByZeroException E)
+            //{
+            //    WriteLine($"Mensagem:{E.Message}");
+            //}
+            //catch (OverflowException E)
+            //{
+            //    WriteLine($"Mensagem:{E.Message}");
+            //}
+            //catch (ArithmeticException E)
+            //{
+            //    WriteLine($"Erro aritmético:{E.Message}");
+            //}
+            catch (Exception E) when (E.Message.ToUpper().Contains("ESTOURO")) 
+            {
+                WriteLine($"Mensagem:{E.Message}");
+            }
+            catch (Exception E) when (E.Message.ToUpper().Contains("ZERO"))
+            {
+                WriteLine($"Mensagem:{E.Message}");
+            }
+        }
+    }
+}
