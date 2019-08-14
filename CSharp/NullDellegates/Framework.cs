8efae252fc95755637def82261a40b40b116b16f
caiomamprin <caiomamprin7@gmail.com>
2019-08-13 22:42:29 -0300

    Exemplo didatico(por isso as classes não estão separadas, para agilizar o exemplo) de como trabalhar com o recurso NullDelegates,implementando a opção de testes para invocação de eventos de tipos delegates de forma a assegurar que os mesmos estejam apontando para um método nomimal, método anônimo ou expressão lambda. Através da sintaxe semelhante ao Null Conditional Operator, juntamente com o método Invoke, torna-se possível invocar um evento sem que o mesmo retorne uma exceção NullReferenceException.

---
+++ CSharp/NullDellegates/Framework.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace Exemplo
+{
+  //public class Boleto
+  //{
+  //  public void Pagar(double valor)
+  //  {
+  //    WriteLine($"Pago valor de {valor} no boleto");
+  //  }
+  //}
+
+  public delegate void PagarEvent(double valor);
+
+  public class Pedido
+  {
+    // Boleto boleto = new Boleto();
+    public event PagarEvent Pagar;
+
+    public void Fechar(double valor)
+    {
+      // delegação com delegate / event
+      Pagar?.Invoke(valor);
+    }  
+  }
+}
---
+++ CSharp/NullDellegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+namespace Exemplo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WriteLine("Informe valor do pedido:");
+      var valor = ToDouble(ReadLine());
+      var pedido = new Pedido();
+      pedido.Pagar += v => WriteLine($"Pago valor de {v} no boleto");
+      pedido.Fechar(valor);
+      ReadLine();
+    }
+
+    //private static void Pedido_Pagar(double valor)
+    //{
+    //  WriteLine($"Pago valor de {valor} no boleto");
+    //}
+  }
+}
