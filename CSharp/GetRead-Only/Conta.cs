95bfaaa6545baf16f08e62d4fbecb7eadd8318eb
caiomamprin <caiomamprin7@gmail.com>
2019-08-13 22:56:24 -0300

    Exemplo didatico(por isso as classes não estão separadas, para agilizar o exemplo) de como trabalhar com a implementação das propriedades automáticas GET / SET, tornando possível criar uma property read-only (somente-leitura) a partir da sua definição. Isso substitui a necessidade de se usar um SET privado (private) para esconder o assessor. Usando apenas o GET, se reforça o encapsulamente sem que para isso seja necessário criar um método de somente-leitura explícito para devolver o conteúdo de um field, que ainda pode ser encapsulado por uma propriedade GET sem o SET. Torna a implementação de propriedades de classes ainda mais enxuta.

---
+++ CSharp/GetRead-Only/Conta.cs
@@ -0,0 +1,28 @@
+﻿namespace Exemplo
+{
+  public class Conta
+  {
+    // antes do C# 6.0
+    // public double Saldo { get; private set; } = 1000;
+    // agora no C# 6.0
+    private double _saldo; // armazena
+    public double Saldo { get { return _saldo; } } // mapeamento
+    public string Cliente { get; set; }
+    public void Sacar(double valor)
+    {
+      _saldo = _saldo - valor;
+    }
+    public void Depositar(double valor)
+    {
+      _saldo = _saldo + valor;
+    }
+    public Conta()
+    {
+      // Saldo = 1000;
+    }
+    //public double getSaldo()
+    //{
+    //  return Saldo;
+    //}
+  }
+}
---
+++ CSharp/GetRead-Only/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace Exemplo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var c = new Conta()
+      {
+        Cliente = "Guinther"
+      };
+      c.Depositar(2000);
+      // c.Saldo = 1000000;
+      WriteLine($"Prezado {c.Cliente}, seu saldo é {c.Saldo}");
+      ReadLine();
+    }
+  }
+}
