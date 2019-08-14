7d2c3ec1f9ca67ac7c5f256d49a4acbff3a57b35
caiomamprin <caiomamprin7@gmail.com>
2019-08-13 22:38:03 -0300

    Exemplo didatico(por isso as classes não estão separadas, para agilizar o exemplo) de como trabalhar com o recurso Overload. Se um método tem a seguinte assinatura Mostrar(object obj), e outro método tem o mesmo nome porém recebendo outro tipo de objeto Mostrar(Aluno obj), haverá uma ambiguidade, pois Aluno é um objeto, por herança. O C#  conta com um novo algoritmo que em tempo de compilação (e não dinamicamente em runtime) determina sempre o melhor overload a ser chamado, por exemplo, o mais específico ao invés do mais generico.

---
+++ CSharp/Overload/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using static System.Console;
+
+namespace Exemplo
+{
+    public class Aluno: object
+    {
+        public int Matricula { get; set; } = 0;
+        public string Nome { get; set; } = "Guinther";
+        public override string ToString()
+        {
+            return $"Matrícula:{Matricula}-Nome:{Nome}";
+        }
+    }
+
+    public class AlunoEspecial: Aluno
+    {
+        public string Situacao { get; set; } = "Especial";
+        public override string ToString()
+        {
+            return $"Matrícula:{Matricula}-Nome:{Nome}-Situação:{Situacao}";
+        }
+    }
+
+
+    class Program
+    {
+        static void Mostrar(string obj)
+        {
+            WriteLine($"Mostrar(string):{obj}");
+        }
+
+        static void Mostrar(int obj)
+        {
+            WriteLine($"Mostrar(int):{obj}");
+        }
+
+        static void Mostrar(bool obj)
+        {
+            WriteLine($"Mostrar(bool):{obj}");
+        }
+
+        static void Mostrar(object obj)
+        {
+            WriteLine($"Mostrar(object):{obj}");
+        }
+
+        static void Mostrar(Aluno obj)
+        {
+            WriteLine($"Mostrar(Aluno):{obj}");
+        }
+
+        static void Mostrar(AlunoEspecial obj)
+        {
+            WriteLine($"Mostrar(AlunoEspecial):{obj}");
+        }
+
+        static void Main(string[] args)
+        {
+            Mostrar("Ola Mundo");
+            Mostrar(10);
+            Mostrar(false);
+            Mostrar(new Aluno());
+            Mostrar(new AlunoEspecial() { Matricula = 123, Nome = "Pauli" });    
+            ReadLine();
+        }
+    }
+}
