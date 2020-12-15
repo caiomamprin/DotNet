b4ad4d8481377749f40d4eef41280504dce9d94b
caio-mamprin <caio@signainfo.com.br>
2020-12-15 16:22:56 -0300

    Add folder and Archives about Exceptions

---
+++ CursoCSharp/CursoCSharp/Excecoes/PrimeiraExcecao.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Excecoes
+{
+
+    public class Conta
+    {
+        double Saldo;
+
+        public Conta(double saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public void Sacar(double valor)
+        {
+            if ( valor > Saldo)
+            {
+                throw new ArgumentException("Saldo insuficiente.");
+            }
+
+            Saldo -= valor;
+        }
+
+    }
+    class PrimeiraExcecao
+    {
+        public static void Executar()
+        {
+
+
+            Conta conta = new Conta(3_500.00);
+
+            try
+            {
+                conta.Sacar(4_000.00);
+                Console.WriteLine("Saque realizado com sucesso!");
+            }
+            catch (ArgumentException ex) //(Exception ex)
+            {
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Exemplo de sessão encerrada!");
+            }
+        }
+    }
+}
