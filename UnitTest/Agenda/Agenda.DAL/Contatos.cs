8aca56d6e1f3e5d0b8c5c067d84c89a925f73add
caiomamprin <caiomamprin7@gmail.com>
2025-04-23 08:28:08 -0300

    Refatorando o codigo

--- UnitTest/Agenda/Agenda.DAL/Contatos.cs
+++ UnitTest/Agenda/Agenda.DAL/Contatos.cs
@@ -6,32 +6,38 @@ namespace Agenda.DAL
 {
     public class Contatos
     {
+        string _strConnection; 
+        SqlConnection _con;
+
+        public Contatos() 
+        {
+            _strConnection = @"Data Source =.\sqlexpress;Initial Catalog=Agenda;Integrated Security=True;";
+            _con = new SqlConnection(_strConnection);
+        }
+
         public void Adicionar(string id, string nome)
         {
-            string strConnection = @"Data Source =.\sqlexpress;Initial Catalog=Agenda;Integrated Security=True;";
+            
+            _con.Open();
 
-            SqlConnection con = new SqlConnection(strConnection);
-            con.Open();
+            var sql = String.Format("insert into Contato (Id,Nome) values ('{0}', '{1}');", id, nome);
 
-            string sql = String.Format("insert into Contato (Id,Nome) values ('{0}', '{1}');", id, nome);
-
-            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlCommand cmd = new SqlCommand(sql, _con);
 
             cmd.ExecuteNonQuery();
-            con.Close();
+            _con.Close();
         }
 
         public string ObterContato(object id)
         {
-            string strConnection = @"Data Source =.\sqlexpress;Initial Catalog=Agenda;Integrated Security=True;";
 
-            SqlConnection con = new SqlConnection(strConnection);
-            con.Open();
-            string sql = String.Format("SELECT Nome FROM Contato where Id = '{0}';", id);
+            _con.Open();
+            var sql = String.Format("SELECT Nome FROM Contato where Id = '{0}';", id);
 
-            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlCommand cmd = new SqlCommand(sql, _con);
 
             return  cmd.ExecuteScalar().ToString();
+            
         }
     }
 }
